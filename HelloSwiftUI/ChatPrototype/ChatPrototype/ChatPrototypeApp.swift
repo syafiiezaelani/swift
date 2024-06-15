0d2c04ad9729571ba34d3a87c3fa634c4d42dc47
Syafiie Zaelani <syafiiezaelani@gmail.com>
2024-06-15 09:54:52 +0800

    First commit tutorial project

---
+++ HelloSwiftUI/ChatPrototype/ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by Syafiie Zaelani on 15/6/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
